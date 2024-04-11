e3a7b76ad72accfec4f7bcb32dd173eeb6a0a245
PaolaEtec <paola.condori@etec.sp.gov.br>
2024-04-11 12:38:49 -0300

    feat: Projeto Dice Roller

---
+++ DiceRoller/DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        Random rndNumber = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RollerButton_Clicked(object sender, EventArgs e)
+        {
+            RandomNumber.Text = rndNumber.Next(1, (int)dicePicker.SelectedItem+1).ToString();
+        }
+    }
+
+}
